8328b0634a5cf4ae63b1150942602dd29f277a5d
M Gulab Yar <mgulabyaarmgulabyaar@gmail.com>
2026-06-10 19:08:17 +0500

    Final delivery everything is in

--- src/taskpane/components/utils/officeHelpers.ts
+++ src/taskpane/components/utils/officeHelpers.ts
@@ -304,12 +304,12 @@ export const formatExcelRange = async (sheetName: string, rangeAddress: string) 
     const sheet = context.workbook.worksheets.getItem(sheetName);
     const range = sheet.getRange(rangeAddress);
 
-    // range.format.fill.color = "#F3F2F1";
+    range.format.fill.color = "#F3F2F1";
 
     const bottomBorder = range.format.borders.getItem("EdgeBottom");
-    // bottomBorder.color = "#0078d4";
-    // bottomBorder.style = "Continuous";
-    // bottomBorder.weight = "Medium";
+    bottomBorder.color = "#0078d4";
+    bottomBorder.style = "Continuous";
+    bottomBorder.weight = "Medium";
 
     await context.sync();
   });
